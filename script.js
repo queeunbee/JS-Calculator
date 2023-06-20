b768258202caacaef7ba552622542c3bb29b2f80
EB <sheunbee@gmail.com>
2023-06-20 00:00:36 -0400

    calc start

--- script.js
+++ script.js
@@ -43,3 +43,12 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerTExt)
+    })
+})
